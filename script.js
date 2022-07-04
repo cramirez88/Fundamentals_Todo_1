00c59f89920a2ed15bbfb844c8502188bdeac909
Christian Ramirez <christian.r0602@gmail.com>
2022-07-04 18:35:47 -0500

    printing

--- script.js
+++ script.js
@@ -14,3 +14,11 @@
 // myName = temp
 // console.log(myNumber)
 // console.log(myName)
+
+
+// Print -52 to 1066
+// Print integers from -52 to 1066 using a FOR loop.
+
+// for (let i = -52; i < 1066; i++){
+//   console.log(i)
+// }
