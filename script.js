b0fb2993d5f4a3c2e1894a32d12b4c08869b11c5
Christian Ramirez <christian.r0602@gmail.com>
2022-07-04 18:32:42 -0500

    swapping and setting

---
+++ script.js
@@ -0,0 +1,16 @@
+// Fundamentals To Do 1
+// OK Ninjas-in-training, use your new knowledge. Can you solve these?
+
+ 
+
+// Setting and Swapping
+// Set myNumber to 42. Set myName to your name. Now swap myNumber into myName & vice versa.
+
+// let myNumber = 42
+// let myName = 'Christian'
+// let temp = myNumber
+
+// myNumber = myName
+// myName = temp
+// console.log(myNumber)
+// console.log(myName)
