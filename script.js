e42b88c6a2289653d61ba137951a0126429877af
Christian Ramirez <christian.r0602@gmail.com>
2022-07-04 18:43:56 -0500

    multiples of three

--- script.js
+++ script.js
@@ -36,3 +36,13 @@
 
 
 // beCheerful()
+
+// Multiples of Three – but Not All
+// Using FOR, print multiples of 3 from -300 to 0. Skip -3 and -6.
+
+// for (let i = -300; i <=0; i+=3){
+//   if(i === -3 || i === -6){
+//     continue
+//   }
+//   console.log(i)
+// }
