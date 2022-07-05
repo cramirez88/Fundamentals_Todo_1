13c8288455dd525b7a94be2b9c7efee567a313f4
Christian Ramirez <christian.r0602@gmail.com>
2022-07-04 20:45:34 -0500

    dojo

--- script.js
+++ script.js
@@ -107,3 +107,14 @@
 
 // Counting, the Dojo Way
 // Print integers 1 to 100. If divisible by 5, print "Coding" instead. If by 10, also print " Dojo".
+
+for (let i = 1; i <= 100; i++){
+
+  if(i % 5 === 0 && i % 10 === 0){
+    
+    console.log('Dojo' + i)
+  }else if(i % 5 === 0){
+    
+    console.log('Coding' + i)
+  }
+}
