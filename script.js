f21d6691fa7ea58235af2f6509bb50dc90cebec2
Christian Ramirez <christian.r0602@gmail.com>
2022-07-05 09:46:53 -0500

    final countdown

--- script.js
+++ script.js
@@ -165,3 +165,23 @@
 // flexibleCountdown(10, 90, 20)
 
 
+
+// The Final Countdown
+// This is based on “Flexible Countdown”. The parameter names are not as helpful, but the problem is essentially identical; don’t be thrown off! Given 4 parameters (param1,param2,param3,param4), print the multiples of param1, starting at param2 and extending to param3. One exception: if a multiple is equal to param4, then skip (don’t print) it. Do this using a WHILE. Given (3,5,17,9), print 6,12,15 (which are all of the multiples of 3 between 5 and 17, and excluding the value 9).
+
+
+// function finalCountdown(param1, param2, param3, param4){
+//   let x = param2
+//   while(x < param3){
+//     x++
+//     if(x % param1 === 0 && x !== param4 ){
+//       console.log(x)
+//     }
+    
+    
+//   }
+// }
+
+// finalCountdown(3,5,17,9)
+
+
