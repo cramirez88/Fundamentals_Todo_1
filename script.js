dddb1a32291633d8ca53e119d52f21b63483f1ff
Christian Ramirez <christian.r0602@gmail.com>
2022-07-05 09:25:11 -0500

    flexible countdown

--- script.js
+++ script.js
@@ -150,4 +150,18 @@
 //   console.log(num)
 // }
 
+// Flexible Countdown
+// Based on earlier “Countdown by Fours”, given lowNum, highNum, mult, print multiples of mult from highNum down to lowNum, using a FOR. For (2,9,3), print 9 6 3 (on successive lines).
+
+// function flexibleCountdown(lowNum, highNum, mult){
+//   for (let i = highNum; i >= lowNum; i--){
+
+//     if(i % mult === 0){
+//       console.log(i)
+//     }
+//   }
+// }
+
+// flexibleCountdown(10, 90, 20)
+
 
