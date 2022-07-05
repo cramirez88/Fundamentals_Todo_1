e89915ad2a8ed419a829525e24b5a62ce5f2cafb
Christian Ramirez <christian.r0602@gmail.com>
2022-07-04 20:34:31 -0500

    print and count

--- script.js
+++ script.js
@@ -75,3 +75,20 @@
 // }
 
 // console.log(leapYear(2021))
+
+
+// Print and Count
+// Print all integer multiples of 5, from 512 to 4096. Afterward, also log how many there were.
+
+// let total = 0
+
+// for (let i = 512; i <= 4096; i++){
+
+//   if (i % 5 === 0){
+//     // console.log(i)
+//     total++
+//   }
+  
+// }
+// console.log(total)
+
