a0578607a1d4af60ee9c16449510f5c64974c60f
Christian Ramirez <christian.r0602@gmail.com>
2022-07-04 20:15:22 -0500

    while loop

--- script.js
+++ script.js
@@ -46,3 +46,12 @@
 //   }
 //   console.log(i)
 // }
+
+// Printing Integers with While
+// Print integers from 2000 to 5280, using a WHILE.
+
+// let num = 2000
+// while(num <= 5280){
+//   console.log(num)
+//   num++
+// }
